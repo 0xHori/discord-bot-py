66a8ae14bb41d32a065dec2559dde881951d0f75
0xHori <81930932+0xHori@users.noreply.github.com>
2025-08-29 17:36:17 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import os
+from dotenv import load_dotenv
 import disnake
+from disnake.ext import commands
+
+load_dotenv()
+
+TOKEN: str = os.environ["TOKEN"]
+
+bot = commands.InteractionBot(test_guilds=[1385772851587715092], sync_commands_debug=True)
+
+@bot.event
+async def on_ready():
+    print("The bot is ready!")
+
+
+bot.load_extension("cogs.ping")
+
+
+bot.run(TOKEN)
+
+
